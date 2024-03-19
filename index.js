22c48ea3dba7780b9f2e2812bdddb781846e47ca
JokastDW <146822037+JokastDW@users.noreply.github.com>
2024-03-19 13:01:01 +0100

    Update index.js
    My code

--- index.js
+++ index.js
@@ -1,71 +1,69 @@
 const express = require('express');
 const axios = require('axios');
+const pug = require('pug');
+const dotenv = require('dotenv');
+
+dotenv.config();
 const app = express();
+app.set('view engine', 'pug'); // Set the view engine to use Pug
 
-app.set('view engine', 'pug');
-app.use(express.static(__dirname + '/public'));
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
+const compiledFunction = pug.compileFile('./views/homepage.pug');
 
-// * Please DO NOT INCLUDE the private app access token in your repo. Don't do this practicum in your normal account.
-const PRIVATE_APP_ACCESS = '';
+const private_app_token = process.env.PRIVATE_APP_TOKEN;
 
-// TODO: ROUTE 1 - Create a new app.get route for the homepage to call your custom object data. Pass this data along to the front-end and create a new pug template in the views folder.
-
-// * Code for Route 1 goes here
-
-// TODO: ROUTE 2 - Create a new app.get route for the form to create or update new custom object data. Send this data along in the next route.
-
-// * Code for Route 2 goes here
-
-// TODO: ROUTE 3 - Create a new app.post route for the custom objects form to create or update your custom object data. Once executed, redirect the user to the homepage.
-
-// * Code for Route 3 goes here
-
-/** 
-* * This is sample code to give you a reference for how you should structure your calls. 
-
-* * App.get sample
-app.get('/contacts', async (req, res) => {
-    const contacts = 'https://api.hubspot.com/crm/v3/objects/contacts';
+app.get('/homepage', async (req, res) => {
+    const companiesEndpoint = 'https://api.hubspot.com/crm/v3/objects/companies'; 
+    const contactsEndpoint = 'https://api.hubspot.com/crm/v3/objects/contacts';
     const headers = {
-        Authorization: `Bearer ${PRIVATE_APP_ACCESS}`,
+        Authorization: `Bearer ${private_app_token}`,
         'Content-Type': 'application/json'
     }
     try {
-        const resp = await axios.get(contacts, { headers });
-        const data = resp.data.results;
-        res.render('contacts', { title: 'Contacts | HubSpot APIs', data });      
+        const response = await axios.get(contactsEndpoint, { headers });
+        const contactsData = response.data;
+        const response2 = await axios.get(companiesEndpoint, { headers });
+        const companiesData = response2.data;
+
+        console.log(contactsData, companiesData);
+        res.render('homepage', { title: 'Hubspot Test', message: 'Contact and Company', contacts: contactsData, companies: companiesData  });
     } catch (error) {
         console.error(error);
+        res.status(500).send('Internal Server Error');
     }
 });
-
-* * App.post sample
-app.post('/update', async (req, res) => {
-    const update = {
-        properties: {
-            "favorite_book": req.body.newVal
-        }
-    }
-
-    const email = req.query.email;
-    const updateContact = `https://api.hubapi.com/crm/v3/objects/contacts/${email}?idProperty=email`;
+app.get('/company', async (req, res) => {
+    const companiesEndpoint = 'https://api.hubspot.com/crm/v3/objects/companies';
     const headers = {
-        Authorization: `Bearer ${PRIVATE_APP_ACCESS}`,
+        Authorization: `Bearer ${private_app_token}`,
         'Content-Type': 'application/json'
-    };
-
-    try { 
-        await axios.patch(updateContact, update, { headers } );
-        res.redirect('back');
-    } catch(err) {
-        console.error(err);
     }
+    try {
+        const response = await axios.get(companiesEndpoint, { headers });
+        const companiesData = response.data;
 
+        console.log(companiesData);
+        res.render('company', { title: 'Hubspot Companies', companies: companiesData });
+    } catch (error) {
+        console.error(error);
+        res.status(500).send('Internal Server Error');
+    }
 });
-*/
+app.get('/contact', async (req, res) => {
+    const contactsEndpoint = 'https://api.hubspot.com/crm/v3/objects/contacts';
+    const headers = {
+        Authorization: `Bearer ${private_app_token}`,
+        'Content-Type': 'application/json'
+    }
+    try {
+        const response = await axios.get(contactsEndpoint, { headers });
+        const contactsData = response.data;
 
+        console.log(contactsData);
+        res.render('contact', { title: 'TestJokast contact', message: 'Banby', contacts: contactsData });
+    } catch (error) {
+        console.error(error);
+        res.status(500).send('Internal Server Error');
+    }
+});
 
-// * Localhost
 app.listen(3000, () => console.log('Listening on http://localhost:3000'));
